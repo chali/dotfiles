181520cead00941f2f13fab2ab3f3dc0210d6576
Martin Chalupa <mchalupa@netflix.com>
2025-06-25 14:50:09 -0700

    rename gh to gho and instal gh cli

--- git/gh.zsh
+++ git/gh.zsh
@@ -1,45 +1,45 @@
-gh_help(){
-    echo "Usage: gh <subcommand> [options]\n"
+gho_help(){
+    echo "Usage: gho <subcommand> [options]\n"
     echo "Subcommands:"
     echo "    browse   Open a GitHub project page in the default browser"
     echo "    cd       Go to the directory of the specified repository"
     echo "    clone    Clone a remote repository"
     echo ""
     echo "For help with each subcommand run:"
-    echo "gh <subcommand> -h|--help"
+    echo "gho <subcommand> -h|--help"
     echo ""
 }
   
-gh_browse() {
+gho_browse() {
     open `git remote -v | awk '/fetch/{print $2}' | sed -Ee 's#(git@|git://)#http://#' -e 's@com:@com/@'`| head -n1
 }
   
-gh_clone() {
+gho_clone() {
     if [[ "$1" == corp/* ]]; then
         : ${GHE_HOST?"GHE_HOST must be set"}
         git clone --recursive "https://$GHE_HOST/$1.git" ~/Projects/github/$1
     else
         git clone --recursive "ssh://git@github.com/$1.git" ~/Projects/github/$1
     fi
-    gh_cd "$1"
+    gho_cd "$1"
 }
 
-gh_cd() {
+gho_cd() {
     cd ~/Projects/github/$1
 }
 
-gh() {
+gho() {
     subcommand=$1
     case $subcommand in
         "" | "-h" | "--help")
-            gh_help
+            gho_help
             ;;
         *)
             shift
-            gh_${subcommand} $@
+            gho_${subcommand} $@
             if [ $? = 127 ]; then
                 echo "Error: '$subcommand' is not a known subcommand." >&2
-                echo "       Run 'ghs --help' for a list of known subcommands." >&2
+                echo "       Run 'gho --help' for a list of known subcommands." >&2
                 return 1
             fi
             ;;
